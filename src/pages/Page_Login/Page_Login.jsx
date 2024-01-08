59d84f62b6cd354643a404b76f5e7b1ff055c549
dcgbao <giabao515151@gmail.com>
2024-01-08 15:56:48 +0700

    update forms

--- src/pages/Page_Login/Page_Login.jsx
+++ src/pages/Page_Login/Page_Login.jsx
@@ -1,21 +1,185 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/shared/Navbar";
 import Footer from "../../components/shared/Footer";
+import { Box, Button, Container, Paper, TextField } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useNavigate } from "react-router-dom";
 
 export default function Page_Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    navigate("/account/transactions");
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <Navbar />
-      <div
-        style={{
-          minHeight: "575px",
+      <Box
+        sx={{
+          height: "78vh",
+          width: "100%",
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <h1>Page_Login</h1>
-      </div>
+        <Container
+          sx={{
+            width: "525px",
+            marginTop: 23,
+          }}
+        >
+          <Paper
+            sx={{
+              boxShadow: 10,
+              borderRadius: 3,
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              padding: 4,
+            }}
+          >
+            <Container>
+              <form onSubmit={handleLogin}>
+                <Box
+                  sx={{
+                    fontSize: 32,
+                    fontWeight: 700,
+                    marginBottom: 1,
+                    textAlign: "center",
+                    color: "#0284c7",
+                  }}
+                >
+                  BKpay
+                </Box>
+                <Box
+                  sx={{
+                    fontSize: 24,
+                    fontWeight: 700,
+                    marginBottom: 3,
+                    textAlign: "center",
+                  }}
+                >
+                  Login
+                </Box>
+                <Box>
+                  <h4 style={{ marginBottom: 5, fontWeight: 600 }}>
+                    Email Address
+                  </h4>
+                  <TextField
+                    label="Enter your email address"
+                    variant="outlined"
+                    type="text"
+                    // size="small"
+                    fullWidth
+                    required
+                    autoComplete="new-text"
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></TextField>
+                  <h4
+                    style={{ marginTop: 25, marginBottom: 5, fontWeight: 600 }}
+                  >
+                    Password
+                  </h4>
+                  <FormControl
+                    variant="outlined"
+                    // size="small"
+                    fullWidth
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  >
+                    <InputLabel htmlFor="outlined-adornment-password">
+                      Enter your password
+                    </InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-password"
+                      type={showPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Enter your password"
+                    />
+                  </FormControl>
+
+                  <Box className="row">
+                    <Box
+                      className="col-6"
+                      sx={{
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        paddingTop: 1,
+                      }}
+                    >
+                      <a href="/" style={{ color: "#0284c7", fontWeight: 500 }}>
+                        Forgot your password?
+                      </a>
+                    </Box>
+                  </Box>
+
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                    sx={{
+                      width: "100%",
+                      borderRadius: "4px",
+                      marginTop: 4,
+                      marginBottom: 2,
+                      textTransform: "none",
+                      backgroundColor: "#0284c7",
+                      fontSize: "16px",
+                    }}
+                  >
+                    Log In
+                  </Button>
+                  <Box sx={{ textAlign: "center" }}>
+                    Don't have an account?
+                    <a
+                      href="/signup"
+                      style={{
+                        color: "#0284c7",
+                        fontWeight: 600,
+                        marginLeft: 4,
+                      }}
+                    >
+                      Sign up here
+                    </a>
+                  </Box>
+                </Box>
+              </form>
+            </Container>
+          </Paper>
+        </Container>
+      </Box>
       <Footer />
     </>
   );
--- src/pages/Page_Signup/Page_Signup.jsx
+++ src/pages/Page_Signup/Page_Signup.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
 import Navbar from "../../components/shared/Navbar";
 import Footer from "../../components/shared/Footer";
-import { Box, Button, Container, Grid, Paper, TextField } from "@mui/material";
+import { Box, Button, Container, Paper, TextField } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { useNavigate } from "react-router-dom";
 
 export default function Page_Signup() {
-  const [firstname, setName] = useState("");
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <Navbar />
@@ -24,7 +44,7 @@ export default function Page_Signup() {
       >
         <Container
           sx={{
-            width: "35%",
+            width: "525px",
             marginTop: 23,
           }}
         >
@@ -36,11 +56,11 @@ export default function Page_Signup() {
               height: "100%",
               display: "flex",
               flexDirection: "column",
-              padding: 5,
+              padding: 4,
             }}
           >
             <Container>
-              <form>
+              <form onSubmit={handleSignup}>
                 <Box
                   sx={{
                     fontSize: 32,
@@ -63,51 +83,69 @@ export default function Page_Signup() {
                   Signup
                 </Box>
                 <Box>
-                  <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                      <h4 style={{ marginBottom: 5, fontWeight: 600 }}>Name</h4>
-                      <TextField
-                        label="Enter your name"
-                        variant="outlined"
-                        type="text"
-                        size="small"
-                        fullWidth
-                        required
-                        autoComplete="new-text"
-                        onChange={(e) => setName(e.target.value)}
-                      ></TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <h4 style={{ marginBottom: 5, fontWeight: 600 }}>
-                        Email Address
-                      </h4>
-                      <TextField
-                        label="Enter your email address"
-                        variant="outlined"
-                        type="text"
-                        size="small"
-                        fullWidth
-                        required
-                        autoComplete="new-text"
-                        onChange={(e) => setEmail(e.target.value)}
-                      ></TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <h4 style={{ marginBottom: 5, fontWeight: 600 }}>
-                        Password
-                      </h4>
-                      <TextField
-                        label="Enter your password"
-                        variant="outlined"
-                        type="password"
-                        size="small"
-                        fullWidth
-                        required
-                        autoComplete="new-password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      ></TextField>
-                    </Grid>
-                  </Grid>
+                  <h4 style={{ marginBottom: 5, fontWeight: 600 }}>Name</h4>
+                  <TextField
+                    label="Enter your name"
+                    variant="outlined"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    autoComplete="new-text"
+                    onChange={(e) => setName(e.target.value)}
+                  ></TextField>
+                  <h4
+                    style={{ marginTop: 25, marginBottom: 5, fontWeight: 600 }}
+                  >
+                    Email Address
+                  </h4>
+                  <TextField
+                    label="Enter your email address"
+                    variant="outlined"
+                    type="text"
+                    size="small"
+                    fullWidth
+                    required
+                    autoComplete="new-text"
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></TextField>
+                  <h4
+                    style={{ marginTop: 25, marginBottom: 5, fontWeight: 600 }}
+                  >
+                    Password
+                  </h4>
+                  <FormControl
+                    variant="outlined"
+                    size="small"
+                    fullWidth
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  >
+                    <InputLabel htmlFor="outlined-adornment-password">
+                      Enter your password
+                    </InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-password"
+                      type={showPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPassword ? (
+                              <Visibility fontSize="small" />
+                            ) : (
+                              <VisibilityOff fontSize="small" />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      label="Enter your password"
+                    />
+                  </FormControl>
                   <Button
                     type="submit"
                     variant="contained"
@@ -116,6 +154,7 @@ export default function Page_Signup() {
                       width: "100%",
                       borderRadius: "4px",
                       marginTop: 4,
+                      marginBottom: 2,
                       textTransform: "none",
                       backgroundColor: "#0284c7",
                       fontSize: "16px",
@@ -123,6 +162,19 @@ export default function Page_Signup() {
                   >
                     Sign Up
                   </Button>
+                  <Box sx={{ textAlign: "center" }}>
+                    Already have an account?
+                    <a
+                      href="/login"
+                      style={{
+                        color: "#0284c7",
+                        fontWeight: 600,
+                        marginLeft: 4,
+                      }}
+                    >
+                      Log in here
+                    </a>
+                  </Box>
                 </Box>
               </form>
             </Container>
